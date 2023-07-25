4ee425a290cb7e1ee9a6a12ca28220c0acac2476
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-07-25 22:40:09 +0300

    profile

---
+++ src/profile/Profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import UserData from '../profile/user.json';
+
+const Profile = () => {
+  const { username, tag, location, avatar, stats } = UserData;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views} </span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
