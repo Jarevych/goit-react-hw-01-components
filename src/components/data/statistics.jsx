8b14232845fe3b28bfb0edfaa26a4b49f67c51aa
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-07-29 21:16:52 +0300

    stat style

--- src/components/data/statistics.jsx
+++ src/components/data/statistics.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StatItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  background-color: ${(props) => randomColor()};
+  font-size: 18px;
+  line-height: 1.2;
+  color: #FFF;
+  padding: 10px 15px;
+  width: 60px;
+  .label {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+  .quantity {
+    font-size: 24px;
+    font-weight: bold;
+  }
+`
+const randomColor = () => {
+  const randomValue = () => Math.floor(Math.random() * 255);
+  return `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`;
+};
 
 const Stat = ({ label, percentage }) => {
   return (
-    <li class="item">
+    <StatItem>
       <span class="label">{label}</span>
-      <span class="percentage">{percentage}</span>
-    </li>
+      <span class="percentage">{percentage}%</span>
+    </StatItem>
   );
 };
 export default Stat;
--- src/components/statistics/stat.jsx
+++ src/components/statistics/stat.jsx
@@ -1,22 +1,25 @@
 import Stat from '../data/statistics';
+import styled from 'styled-components';
+
+const StatContainer = styled.div`
+  display: flex;
+`;
+const StatList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  padding: 0;
+  `;
 
 const Statistic = ({ statData }) => {
   return (
-    <div>
-    <ul>
-      {statData.map(({ id, label, percentage }) => {
-          return (
-            <Stat
-              id={id}
-              label={label}
-              percentage={percentage}
-            />
-          );
-        })
-      }
-    </ul>
-    </div>
+    <StatContainer>
+      <StatList>
+        {statData.map(({ id, label, percentage }) => {
+          return <Stat id={id} label={label} percentage={percentage} />;
+        })}
+      </StatList>
+    </StatContainer>
   );
 };
 
-export default Statistic
+export default Statistic;
